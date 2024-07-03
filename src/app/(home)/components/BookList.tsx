71c52211f2e58211e4ddfa2c241b7cb8e6f86b71
Naman Pandey <2492004namanpandey@gmail.com>
2024-07-03 07:33:57 +0530

    add suspense

--- src/app/(home)/components/BookList.tsx
+++ src/app/(home)/components/BookList.tsx
@@ -2,7 +2,15 @@ import { Book } from "@/types";
 import React from "react";
 import BookCard from "./BookCard";
 
-const BookList = ({ books }: { books: Book[] }) => {
+const BookList = async () => {
+  // data fetching
+  const response = await fetch(`${process.env.BACKEND_URL}/books`, { cache: 'no-store' });
+  if (!response.ok) {
+    throw new Error("An error occured while fetching books");
+  }
+  const books = await response.json();
+
+
   return (
     <div className="grid grid-cols-1 gap-8 md:grid-cols-3 max-w-7xl mx-auto mb-10">
       {books.map((book: Book) => (
